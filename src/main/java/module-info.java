eba1d0072d79afa9c5fc58f641db046449736a5f
hernj9 <116303299+hernj9@users.noreply.github.com>
2022-11-03 23:37:05 -0400

    Delete module-info.java

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-/* doesn't work with source level 1.8:
-module com.mycompany.fxmlexample {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    opens com.mycompany.fxmlexample to javafx.fxml;
-    exports com.mycompany.fxmlexample;
-}
-*/
